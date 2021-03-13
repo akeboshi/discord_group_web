d6283f63aad330cb79ff878c51c9eddd70b65837
akeboshi <isao-aruga@bm-sms.co.jp>
2021-03-13 13:14:48 +0900

    fix dom load tyming

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,15 +8,12 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
+const app = new Vue({
+  render: h => h(App)
+}).$mount()
+document.body.appendChild(app.$el)
 
-  console.log(app)
-})
-
+console.log(app)
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
